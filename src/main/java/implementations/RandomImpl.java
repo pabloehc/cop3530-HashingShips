da5c78c85da4e8c4cdbe0ff212dfadcce0b51276
sgonz477 <122726425+sgonz477@users.noreply.github.com>
2023-04-01 17:54:08 -0400

    add randomLives method (#5)
    added randomLives method 
    tested input manually

---
+++ src/main/java/implementations/RandomImpl.java
@@ -0,0 +1,51 @@
+package implementations;
+
+import java.util.ArrayList;
+import java.util.Random;
+import interfaces.RandomShips;
+import java.util.List;
+
+public class RandomImpl implements RandomShips {
+
+    public static final Random rand = new Random();
+
+
+    public static final int MIN_LIVES = 1;
+    public static final int MAX_LIVES = 5;
+    public static final int MIN_SHIPS = 1;
+    public static final int MAX_SHIPS = 5;
+
+
+    /**
+     * Create a list of strings that has 2 random characters from a-z.
+     * @param numberOfShips is the length of the list.
+     * @return a list of strings.
+     */
+    @Override
+    public List<String> randomValues(int numberOfShips) {
+        //Input: 2; Output: [ab, bk]
+        return null;
+
+    }
+
+    /**
+     * Creates a list of length numberOfShips up to MAX_SHIPS.
+     * Where each value is a random number from MIN_LIVES to MAX_LIVES.
+     * @param numberOfShips is the length of the list.
+     * @return a list of integers.
+     */
+    @Override
+    public List<Integer> randomLives(int numberOfShips) {
+
+        if (numberOfShips> MAX_SHIPS || numberOfShips < MIN_SHIPS) {
+            return null;
+        }
+        List<Integer> randomLives = new ArrayList<>();
+
+        for (int i=0; i < numberOfShips; i++){
+            int randomNumber = rand.nextInt(MAX_LIVES) + MIN_LIVES; // generates a random integer between 1 and 5 (inclusive)
+            randomLives.add(randomNumber);
+        }
+        return randomLives;
+    }
+}
